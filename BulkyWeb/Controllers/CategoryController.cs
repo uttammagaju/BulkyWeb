3749b8064355ceb71a87ee6b1786cc142e59c261
uttammagaju <108165184+uttammagaju@users.noreply.github.com>
2024-04-30 16:37:40 +0545

    Complete Finished up to Catagory Edit

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -1,12 +1,76 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BulkyWeb.Data;
+using BulkyWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace BulkyWeb.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
+        {
+           List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+        [HttpGet]
+        public IActionResult Create()
         {
             return View();
         }
+        [HttpPost]
+        public IActionResult Create(Category emodel)
+        {
+            if(emodel.Name == emodel.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name","The DisplayOrder cannot exactly match the Name.");
+            }
+            if (emodel.Name == "test")
+            {
+               ModelState.AddModelError("name", "Test is an invalid value");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(emodel);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            
+            return View(emodel);
+           
+        }
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            if(id == 0 || id == null)
+            {
+                return NotFound();
+            }
+            Category? categoryFromDb = _db.Categories.Find(id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category emodel)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(emodel);
+                _db.SaveChanges();
+            }
+            if (emodel == null)
+            {
+                return NotFound(ModelState);
+            }
+            return RedirectToAction("Index");
+        }
+
     }
 }
--- BulkyWeb/Models/Category.cs
+++ BulkyWeb/Models/Category.cs
@@ -1,13 +1,18 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace BulkyWeb.Models
 {
     public class Category
     {
         [Key]
-        public int ID { get; set; }
+        public int ID { get; set; } 
         [Required]
+        [DisplayName("Name")]
+        [MaxLength(50)]
         public String Name { get; set; }
+        [DisplayName("Display Order")]
+        [Range(1,100)]
         public int DisplayOrder { get; set; }
     }
 }
